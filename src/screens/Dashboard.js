c76e8f6e53de618b5455b802747cd317c89ce4bd
bigleap2019 <askthebigleap@gmail.com>
2024-08-26 12:57:46 +0530

    issue fix

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -150,7 +150,7 @@ const Dashboard = ({navigation}) => {
           )}
 
           <PointCard
-            imageLink={image ? image : defaultImage}
+            imageLink={image ? image : profileImageLink}
             points={points}
             pointLimit={pointLimit.limit}
             pColor={pColor}
